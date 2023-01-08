1b1c73b872e00d0ba8b6ff0ef128d9b4f44ad98d
Mochamad Idris <idrez.mochamad@gmail.com>
2023-01-08 22:41:46 +0700

    perubahan pada activity_main.xml

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -1,33 +1,52 @@
 package com.example.ujianandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView lvNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        lvNama = findViewById(R.id.lvNama);
 
-        ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+        ArrayList<String> daftar_nama = getIntent().getStringArrayListExtra("daftar_nama");
 
-        if(daftar_nama.isEmpty()){
-            daftar_nama.add("data masih kosong");
+        ArrayList<String> filteredList = new ArrayList<>();
+
+        if (daftar_nama != null && !daftar_nama.isEmpty()) {
+            for (String nama : daftar_nama) {
+                // Mengekstrak angka index dan nama
+                String[] parts = nama.split("\\. ");
+                int index = Integer.parseInt(parts[0]);
+                String name = parts[1];
+
+                // Memeriksa apakah nama memiliki nama depan dan belakang
+                String[] nameParts = name.split(" ");
+                if (nameParts.length >= 2) {
+                    String namaLengkap = nameParts[0] + " " + nameParts[1];
+                    filteredList.add(index + ". " + namaLengkap);
+                }
+            }
         }
 
-        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+        ArrayAdapter<String> ad_nama;
+        if (!filteredList.isEmpty()) {
+            ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filteredList);
+        } else {
+            filteredList.add("Data masih kosong");
+            ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filteredList);
+        }
 
         lvNama.setAdapter(ad_nama);
-
     }
 }
--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -2,44 +2,56 @@ package com.example.ujianandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ListView;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edNamaDepan;
+    private EditText edNamaBelakang;
+    private Button btnSimpan;
+    private ListView lvNama;
+    private ArrayList<String> daftar_nama = new ArrayList<>();
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edNamaDepan = (EditText) findViewById(R.id.edNamaDepan);
-        EditText edNamaBelakang = (EditText) findViewById(R.id.edNamaBelakang);
-        Button btnSimpan = (Button) findViewById(R.id.btnSimpan);
+        edNamaDepan = findViewById(R.id.edNamaDepan);
+        edNamaBelakang = findViewById(R.id.edNamaBelakang);
+        btnSimpan = findViewById(R.id.btnSimpan);
 
-        ArrayList<String> daftar_nama = new ArrayList<>();
+        lvNama = findViewById(R.id.lvNama);
 
         Intent intent_list = new Intent(MainActivity.this, ListActivity.class);
 
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String isian_nama_depan = edNamaDepan.getText().toString();
-                String isian_nama_belakang = edNamaBelakang.getText().toString();
+                String isian_nama_depan = edNamaDepan.getText().toString().trim();
+                String isian_nama_belakang = edNamaBelakang.getText().toString().trim();
 
-                if(isian_nama_depan.isEmpty()){
+                if (isian_nama_depan.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                } else {
+                    String nama_lengkap = isian_nama_depan + " " + isian_nama_belakang;
                     daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
-                    edNamaDepan.setText("");
-                    edNamaBelakang.setText("");
+                    for (int i = 1; i <= 20; i++) {
+                        if (i % 2 != 0) { // Memeriksa bilangan ganjil
+                            daftar_nama.add(i + ". " + nama_lengkap);
+                        }
+                    }
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
                     startActivity(intent_list);
                 }
